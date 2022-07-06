488e67ffc480b8c0f269e335faeb697aaf732cc6
EduardoC66 <edudaniel264@gmail.com>
2022-07-05 21:33:02 -0400

    ql

--- lib/pages/home/presentation/home.dart
+++ lib/pages/home/presentation/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart';
+import 'package:vogon_chat/pages/home/home_store.dart';
+
+final store = GetIt.I.get<HomeStore>();
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -10,6 +15,32 @@ class HomeView extends StatelessWidget {
         centerTitle: true,
         title: Text("Vogon Chat"),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (_) {
+                  return SimpleDialog(
+                    children: [
+                      const Text('Adicionar Contato'),
+                      TextFormField(
+                        controller: store.adicionarContatoController,
+                        decoration: const InputDecoration(
+                          hintText: 'Nome do Contato',
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          store.adicionarNovoContato();
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Salvar'),
+                      ),
+                    ],
+                  );
+                });
+          }),
     );
   }
 }
